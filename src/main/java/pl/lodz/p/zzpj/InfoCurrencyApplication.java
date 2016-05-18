cc4ef623ff89ed54e3871251bfe841f0a3592ecd
Piotr Węglewski <190160@edu.p.lodz.pl>
2016-05-18 22:16:02 +0200

    BE project created with spring initializr

---
+++ src/main/java/pl/lodz/p/zzpj/InfoCurrencyApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.zzpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoCurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoCurrencyApplication.class, args);
+	}
+}
