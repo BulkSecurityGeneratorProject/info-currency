30005b5f9c64701d8e445be25547680a978fd59e
Piotr Węglewski <190160@edu.p.lodz.pl>
2016-08-19 08:48:32 +0200

    Added maven build profiles: dev and prod

---
+++ src/main/java/config/Constants.java
@@ -0,0 +1,15 @@
+package config;
+
+/**
+ * Application constants.
+ * <p>
+ * Created by piotr on 18.08.16.
+ */
+public final class Constants {
+    // Spring profiles for development and production
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+
+    private Constants() {
+    }
+}
--- src/test/java/pl/lodz/p/zzpj/InfoCurrencyApplicationTests.java
+++ src/test/java/pl/lodz/p/zzpj/InfoCurrencyApplicationTests.java
@@ -11,8 +11,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class InfoCurrencyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
