8a6246f7936bd12266abcb8bbb37bb0641129e9e
Piotr Bugara <189634@edu.p.lodz.pl>
2016-09-02 15:35:28 +0200

    Currency rate search changed

--- src/main/java/pl/lodz/p/zzpj/managers/CurrenciesManager.java
+++ src/main/java/pl/lodz/p/zzpj/managers/CurrenciesManager.java
@@ -1,9 +1,11 @@
 package pl.lodz.p.zzpj.managers;
 
+import pl.lodz.p.zzpj.controllers.vm.CurrencyVM;
 import pl.lodz.p.zzpj.model.Currency;
 import pl.lodz.p.zzpj.model.ExchangeRatesSeries;
 
 public interface CurrenciesManager {
     String getLastCurrenciesXMLFromWebsite();
     ExchangeRatesSeries getDailyCurrencyRateFromWebsite(Currency currencySymbol);
+    ExchangeRatesSeries getCurrencyRate(CurrencyVM request);
 }
